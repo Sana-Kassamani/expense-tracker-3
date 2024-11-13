bdecb922bc727671518ef5832f88c513249c1917
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-13 13:57:08 +0200

    Setup - index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouer } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const rootContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(rootContainer);
 root.render(
-  <BrowserRouer>
+  <BrowserRouter>
     <App />
-  </BrowserRouer>
+  </BrowserRouter>
 );
